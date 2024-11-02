0f04de069ddf90dd6c5ea2d6a44747b1c55846bc
IdaMariaB <2306044@edu.karelia.fi>
2024-11-02 18:06:02 +0200

    Tuotu tiedostot uudelleen

---
+++ App/Program.cs
@@ -0,0 +1,9 @@
+﻿using AppLogger;
+using Humanizer;
+string word = "kissa";
+string luku = 1000.ToWords();
+DateTime datetime = DateTime.Now.AddDays(4);
+string newDate = datetime.Humanize();
+Logger.Log(word);
+Logger.Log(luku);
+Logger.Log(newDate);
