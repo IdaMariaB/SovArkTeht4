8b35bf5cdfeab640fa00ef4f0706989b664bc7f0
IdaMariaB <2306044@edu.karelia.fi>
2024-11-02 18:04:05 +0200

    Delete Tehtava3 directory

--- Tehtava3/App/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using AppLogger;
-using Humanizer;
-string word = "kissa";
-string luku = 1000.ToWords();
-DateTime datetime = DateTime.Now.AddDays(4);
-string newDate = datetime.Humanize();
-Logger.Log(word);
-Logger.Log(luku);
-Logger.Log(newDate);
